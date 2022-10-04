a75113c990f2ff05fbde55f1ff5eff2a51470829
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-10-04 11:30:40 -0300

    feat: req 02 - creates subclass for Halflings with proper maxLifePoints and racesInstances counter

---
+++ src/Races/Halfling.ts
@@ -0,0 +1,25 @@
+import Race from './Race';
+
+class Halfling extends Race {
+  private _maxLifePoints: number;
+  private static _racesInstances = 0;
+
+  constructor(
+    name: string,
+    dexterity: number,
+  ) {
+    super(name, dexterity);
+    this._maxLifePoints = 60;
+    Halfling._racesInstances += 1;
+  }
+
+  public static createdRacesInstances(): number {
+    return Halfling._racesInstances;
+  }
+
+  public get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+}
+
+export default Halfling;
