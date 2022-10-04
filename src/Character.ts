008a46de4c7a6e0b43af8516b5c1bafd6fbd8bab
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-10-04 14:09:06 -0300

    fix: req 09 - adds SimpleFighter as possible enemy of Character's attack

--- src/Character.ts
+++ src/Character.ts
@@ -1,6 +1,6 @@
 import Archetype, { Mage } from './Archetypes';
 import Energy from './Energy';
-import Fighter from './Fighter';
+import Fighter, { SimpleFighter } from './Fighter';
 import Race, { Elf } from './Races';
 import getRandomInt from './utils';
 
@@ -63,7 +63,7 @@ class Character implements Fighter {
     return this._lifePoints;
   }
 
-  public attack(enemy: Fighter): void {
+  public attack(enemy: Fighter | SimpleFighter): void {
     const damage = this._strength;
     enemy.receiveDamage(damage);
   }
