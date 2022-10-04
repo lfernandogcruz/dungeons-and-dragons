da5fedc435bc18da1a292daa33fa693e42794bdc
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-10-04 17:07:43 -0300

    feat: req 13 - creates index file on src directory to use every previously created classes and simulate a battleground

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import Battle, { PVE, PVP } from './Battle';
+import Character from './Character';
+import Dragon from './Dragon';
+import Monster from './Monster';
+
+const player1 = new Character('Presto');
+const player2 = new Character('Xena');
+const player3 = new Character('Galadriel');
+
+player1.levelUp();
+player1.levelUp();
+player1.levelUp();
+player1.levelUp();
+
+const monster1 = new Monster();
+const monster2 = new Dragon();
+
+const pvp = new PVP(player2, player3);
+
+const pve = new PVE(player1, [monster1, monster2]);
+
+function runBattles(battlesArray: Battle[]): void {
+  battlesArray.forEach((battle) => battle.fight());
+}
+
+export {
+  player1,
+  player2,
+  player3,
+  monster1,
+  monster2,
+  pvp,
+  pve,
+  runBattles,
+};
