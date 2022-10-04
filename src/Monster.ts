f3c042836dfb2b0339eee840063b5572e354c887
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-10-04 16:35:11 -0300

    refact: req 12 - changes from private to protected the Monster's attributes

--- src/Monster.ts
+++ src/Monster.ts
@@ -1,8 +1,8 @@
 import { SimpleFighter } from './Fighter';
 
 class Monster implements SimpleFighter {
-  private _lifePoints: number;
-  private _strength: number;
+  protected _lifePoints: number;
+  protected _strength: number;
 
   constructor() {
     this._lifePoints = 85;
